2334893931dad3c7da5937c6f2b9fed507ac722f
Emmanuel <emmanuel.feygelson@gmail.com>
2018-01-23 00:12:42 -0800

    W7D2 correct submission

--- W7D2/Middleware/frontend/todo_redux.jsx
+++ W7D2/Middleware/frontend/todo_redux.jsx
@@ -7,41 +7,34 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  let store = configureStore(preloadedState);
-
-  // Phase 1: Logging
-  // store.dispatch = addLoggingToDispatch(store);
-
-  // Phase 2: Refactoring (without using Redux applyMiddleware)
-  // store = applyMiddlewares(store, addLoggingToDispatch);
+  const store = configureStore(preloadedState);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
 
-// Phase 1: Logging
+
 // const addLoggingToDispatch = (store) => {
-//   const OGDispatch = store.dispatch;
+//   const StoreDispatch = store.dispatch;
 //   return (action) => {
 //     console.log(store.getState());
-//     console.log(action);
-//     OGDispatch(action);
+//     console.log((action));
+//     StoreDispatch(action);
 //     console.log(store.getState());
-//   }
-// }
+//   };
+// };
 
-// Phase 2: Refactoring (without Using Redux applyMiddleware)
-// const addLoggingToDispatch = store => next => action => {
-//   console.log(store.getState());
-//   console.log(action);
-//   next(action);
-//   console.log(store.getState());
-// };
-//
-// const applyMiddlewares = (store, ...middlewares) => {
-//   let dispatch = store.dispatch;
-//   middlewares.forEach((middleware) => {
-//     dispatch = middleware(store)(dispatch);
-//   });
-//   return Object.assign({}, store, { dispatch });
-// };
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
